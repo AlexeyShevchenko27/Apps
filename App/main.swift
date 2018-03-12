de7c9a57074e173a5704b0216c84ccca725c2a39
Я <alexey@alexeys-imac.local>
2018-03-13 00:57:01 +0200

    new

--- App/main.swift
+++ App/main.swift
@@ -8,5 +8,29 @@
 
 import Foundation
 
-print("Hello, World!")
+let n = 9
+let p = 0.54
+let q = 1-p
+func fact(_ z: Double) -> Double
+    {
+        if z == 0
+            {
+                return 1
+            }
+        return z * fact(z-1)
+    }
 
+var eq: Double
+var whole: Double
+var deg: Double
+var shit: Double
+var mass: [Double] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for i in mass
+{
+    eq = Double(fact(9)/(fact(i)*fact(9-i)))
+    deg = Double(truncating: pow(0.54, i) as NSNumber)
+    shit = Double(truncating: pow(0.46, 9-i) as NSNumber)
+    whole = eq*deg*shit
+    print(whole)
+}
